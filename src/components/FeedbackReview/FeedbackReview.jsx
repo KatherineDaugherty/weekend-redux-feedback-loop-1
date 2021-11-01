281e6a65a908d9b913bbe3d23840a1f41091e08e
Katherine Daugherty <katherinedaugherty23@gmail.com>
2021-11-01 10:19:47 -0500

    added a Markup

--- src/components/FeedbackReview/FeedbackReview.jsx
+++ src/components/FeedbackReview/FeedbackReview.jsx
@@ -3,39 +3,39 @@ import axios from 'axios';
 import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
 
-function FeedbackReview () {
+function FeedbackReview() {
 
-    const history = useHistory();
+  const history = useHistory();
 
-    const feedbackInput = useSelector((store) => store.inputReducer);
+  const feedbackInput = useSelector((store) => store.inputReducer);
 
-    //when submit button clicked, post to DB & send to success page
-    const handleSubmit = () => {
+  //when submit button clicked, post to DB & send to success page
+  const handleSubmit = () => {
 
-        console.log('clicked');
-      
-        //post to server
-        axios.post('/feedback', feedbackInput)
-        .then(response => {
+    console.log('clicked');
+
+    //post to server
+    axios.post('/feedback', feedbackInput)
+      .then(response => {
         console.log('posted', response)
-        }).catch (error => {
-        console.log('error', error )
-        })
-        history.push('/success');
-    }
+      }).catch(error => {
+        console.log('error', error)
+      })
+    history.push('/success');
+  }
 
   return (
     <>
-    <h2>Review your feedback</h2>
-    <p>Feelings: {feedbackInput.feeling}</p>
-    <p>Understanding: {feedbackInput.understanding}</p>
-    <p>Support: {feedbackInput.support}</p>
-    <p>Comments: {feedbackInput.comments}</p>
-    
-    <button onClick={handleSubmit}>Submit</button>
-   
-   </>
-  )   
+      <h2>Review your feedback</h2>
+      <p>Feelings: {feedbackInput.feeling}</p>
+      <p>Understanding: {feedbackInput.understanding}</p>
+      <p>Support: {feedbackInput.support}</p>
+      <p>Comments: {feedbackInput.comments}</p>
+
+      <button onClick={handleSubmit}>Submit</button>
+
+    </>
+  )
 
 }
 
